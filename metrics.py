f4c72de04d7ea3ef65cce611411d2b57f0c162d9
Andrey Popov <ap4220@ic.ac.uk>
2022-02-25 00:28:17 +0000

    Added metrics.py which calculates the number of non empty lines of codebase

---
+++ metrics.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+from os.path import isfile, isdir
+
+def file_exists(path):
+	return os.path.exists(path)
+
+
+languages = {
+	".py": "Python",
+	".c": "C Programming Language", 
+	".h": "C header files",
+	".html": "HTML",
+	".kt": "Kotlin",
+    ".java": "JavaScript",
+    ".css": "CSS",
+    ".js": "JavaScript",
+    ".sh": "Bash scripts",
+    ".hs" : "Haskell",
+    ".lhs": "Literate Haskell"
+}
+
+lang_stat = dict(zip(languages.keys(), [0] * len(languages)))
+
+DIRNAME, FILENAME = os.path.split(os.path.abspath(__file__))
+
+SCR_DIR = os.path.join(DIRNAME, "src")
+
+def traverse(root_dir, languages):
+    for path in os.listdir(root_dir):
+        path = os.path.join(root_dir, path)
+        if isdir(path):
+            traverse(path, languages)
+        else:
+            file_ex = None
+            for extension in languages.keys():
+                if (path.endswith(extension)):
+                    file_ex = extension
+                    break
+
+            if file_ex == None:
+                print("unresolved extension of", path)
+                continue
+
+            with open(path, 'r', encoding="latin-1") as f:
+                count = 0
+                for line in f:
+                    if line.rstrip() != "":
+                        lang_stat[file_ex] += 1
+                        count += 1
+                print(path, "-", count)
+
+print()
+traverse(SCR_DIR, languages)
+print()
+
+lang_stat = dict(filter(lambda p: p[1] > 0, lang_stat.items()))
+
+total_line_count = sum(lang_stat.values())
+
+for (ex, lang) in languages.items():
+    if (ex not in lang_stat.keys()):
+        continue
+    print(f"There are {lang_stat[ex]} lines of {lang} in the project, which is {round(100 * lang_stat[ex] / total_line_count)}%")
+    print()
+
+print("Total number of non empty lines is", total_line_count)
+
