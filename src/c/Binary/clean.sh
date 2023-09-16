9c8797511efd8b88bf072ecf3560e28b5210fb6a
Andrey-Kachow <andr10xp@gmail.com>
2023-09-16 07:07:43 +0100

    test commit

--- src/c/Binary/clean.sh
+++
@@ -1,7 +0,0 @@
-echo "Removing executable files..."
-sleep 2
-rm *.exe
-echo "Removing object files..."
-sleep 2
-rm *.obj
-echo "Done."
--- src/c/Dmitry_C/c_tasks/myimpls/list_sort_old_final/test_listimpl.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_LISTIMPL_H
-#define TEST_LISTIMPL_H
-
-void test(void);
-
-#endif
