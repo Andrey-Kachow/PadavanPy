8c31aa1d65345abae63080ccc92ea86e39266e22
Andrey-Kachow <andr10xp@gmail.com>
2023-09-20 17:10:59 +0100

    Added old ascii character subtraction demo

---
+++ src/c/sample_lessons/basic_stuff/ascii/character_arithmetics_demo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    char c = '1';
+
+    printf("%c\n", c);
+    printf("%d\n", c);
+
+    printf("%d\n", '0' - '0');
+    printf("%d\n", '1' - '0');
+    printf("%d\n", '2' - '0');
+    printf("%d\n", '3' - '0');
+    printf("%d\n", '4' - '0');
+    printf("%d\n", '5' - '0');
+    printf("%d\n", '6' - '0');
+
+    return 0;
+}
