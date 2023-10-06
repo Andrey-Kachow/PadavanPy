cdd8085fc6b235a1fde43061da27dcddc4e67d4c
Andrey-Kachow <andr10xp@gmail.com>
2023-10-06 10:13:04 +0100

    fixed compiler errors in struct demo test.c file

--- src/c/by_topic/structs/test.c
+++ src/c/by_topic/structs/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 struct point {
     int x;
@@ -12,13 +13,20 @@ struct animal {
     char name[10];
     int age;
     int is_hungry;
-}
+};
 
 
 struct cat {
     char name[10];
     int age;
     int is_hungry; // 1 - yes 0 - no
+};
+
+struct dog {
+    char name[10];
+    int age;
+    int is_hungry;
+    char breed[10];
 };
 
 struct pet_shop {
@@ -29,19 +37,19 @@ struct pet_shop {
 };
 
 
-int how_many_cats_are_hungry_in_the_city(struct pet_shop shops[], int number_of_pet_shops) {
+int how_many_cats_are_hungry_in_the_city(struct pet_shop *shops[], int number_of_pet_shops) {
 
     int hungry_cats_count = 0;
 
     for (int pet_shop_count = 0; pet_shop_count < number_of_pet_shops; pet_shop_count++) {
         
-        struct pet_shop current = shops[pet_shop_count];
+        struct pet_shop *current = shops[pet_shop_count];
         
-        for (int cat_count = 0; cat_count < current.number_of_cats_to_sale; cat_count++) {
+        for (int cat_count = 0; cat_count < current->number_of_cats_to_sale; cat_count++) {
 
-            struct cat current_cat = current.cats[cat_count];
+            struct cat current_cat = current->cats[cat_count];
             
-            if (cat.is_hungry) {
+            if (current_cat.is_hungry) {
                 hungry_cats_count++;
             }
         }
@@ -52,16 +60,28 @@ int how_many_cats_are_hungry_in_the_city(struct pet_shop shops[], int number_of_
 
 
 
+struct batman {
+    char title[10];
+    char director_name[15];
+    int movie_filmed_date[3];
+};
+
+
+struct batman *get_fitst_batman() {
+    return NULL;
+}
+
+struct batman *get_next_batman(struct batman *batman) {
+    return NULL;
+}
+
+int no_more_batmans(struct batman *batman) {
+    return 1;
+}
+
+
+
 int main(void) {
-
-    int expected = 5;
-    int actual_cats = how_many_cats_are_hungry_in_the_city(...);
-
-    if (expected != actual_cats) {
-        fail();
-    }
-    
-
 
     struct point p = { 10, 20 };
 
@@ -86,12 +106,12 @@ int main(void) {
 
     // {2, 4, 6}
 
-    for (int even_number = 2; even_number < 7; number += 2) {
+    for (int even_number = 2; even_number < 7; even_number += 2) {
 
     }
 
         
-    for (struct batman current_batman_it = get_fitst_batman();
+    for (struct batman *current_batman_it = get_fitst_batman();
             !no_more_batmans(current_batman_it);
             current_batman_it = get_next_batman(current_batman_it)
     ) {
