2a4bd070737cb6150979dc7c8903b501dea22927
Andrey-Kachow <andr10xp@gmail.com>
2023-12-02 23:22:31 +0000

    Modfied sdl2 project

--- src/c/gui_apps/evolution/main.c
+++ src/c/gui_apps/evolution/main.c
@@ -1,6 +1,27 @@
 // #include <SDL2/SDL.h>
 #include <SDL.h>
 
-int main() {
-    return 0;
+SDL_Renderer *renderer;
+SDL_Window *window;
+
+int main(int argc, char *argv[])
+{
+	// memset(&app, 0, sizeof(App));
+
+	// initSDL();
+
+	// atexit(cleanup);
+
+	while (1)
+	{
+		// prepareScene();
+
+		// doInput();
+
+		// presentScene();
+
+		// SDL_Delay(16);
+	}
+
+	return 0;
 }
