92c42591d6aaafccacf986cac7086d676dd42bb9
Andrey-Kachow <andr10xp@gmail.com>
2023-12-04 13:05:47 +0000

    Appended the missing bit to sdl projects

--- src/c/gui_apps/conway/main.c
+++ src/c/gui_apps/conway/main.c
@@ -41,6 +41,10 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+	
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
 	return 0;
 }
--- src/c/gui_apps/evolution/main.c
+++ src/c/gui_apps/evolution/main.c
@@ -42,5 +42,9 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
 	return 0;
 }
