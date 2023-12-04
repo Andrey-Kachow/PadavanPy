84981e8fbd2f493fb26f713c349b1a4b96ea3167
Andrey-Kachow <andr10xp@gmail.com>
2023-12-04 12:56:26 +0000

    Blank window SDL app

--- src/c/gui_apps/evolution/main.c
+++ src/c/gui_apps/evolution/main.c
@@ -1,26 +1,45 @@
 // #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <SDL.h>
+#include <stdbool.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 SDL_Renderer *renderer;
-SDL_Window *window;
 
-int main(int argc, char *argv[])
-{
-	// memset(&app, 0, sizeof(App));
+int main(int argc, char *argv[]) {
 
-	// initSDL();
+	SDL_Window *window = NULL;
 
-	// atexit(cleanup);
+	SDL_Surface *screen_surface = NULL;
 
-	while (1)
-	{
-		// prepareScene();
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initilaize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
-		// doInput();
+	window = SDL_CreateWindow("Evolution Project", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
 
-		// presentScene();
+	screen_surface = SDL_GetWindowSurface(window);
 
-		// SDL_Delay(16);
+	SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 255, 255, 255));
+
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Event event;
+	bool quit = false;
+	while (quit == false) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
 	}
 
 	return 0;
