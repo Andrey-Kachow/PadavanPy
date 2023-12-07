d14ba3ce9beaa454234dbaca918111984da57ea9
Andrey-Kachow <andr10xp@gmail.com>
2023-12-07 08:12:58 +0000

    Extracted SDL boilerplate startup template onto separate file

---
+++ src/c/gui_apps/sdl_boilerplate.c
@@ -0,0 +1,48 @@
+// #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <stdbool.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char *argv[]) {
+
+	SDL_Window *window = NULL;
+
+	SDL_Surface *screen_surface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initilaize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow("SDL App", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	screen_surface = SDL_GetWindowSurface(window);
+
+	SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 255, 255, 255));
+
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Event event;
+	bool quit = false;
+	while (quit == false) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+	return 0;
+}
