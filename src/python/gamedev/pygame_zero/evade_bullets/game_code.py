29d6be5afe414f972d895e18c667c01d8787873e
Andrey-Kachow <andr10xp@gmail.com>
2023-12-07 23:01:00 +0000

    Final update on evade the bullets game

--- src/python/gamedev/pygame_zero/evade_bullets/game_code.py
+++ src/python/gamedev/pygame_zero/evade_bullets/game_code.py
@@ -1,17 +1,21 @@
 import random
 import math
+from pgzhelper import *
 
-HEIGHT = 1000
-WIDTH = 2000
+HEIGHT = 800
+WIDTH = 1600
+
+
+#      % mv ~/Downloads/Ma... music
+
 
 '''
         We agreed that 'L' means LEFT, 'R' means Right
 '''
 
-blue = (0, 128, 247)
+blue = (0, 0, 0)
 Speed = 5
 alien = Actor("alien") 
-
 horizontal_direction = "L" # L or R
 vertical_direction = 'U' # U or D
 Top = "U"
@@ -21,7 +25,16 @@ MOVING = 'M'
 IDLE = 'I'
 player_motion_state_h = IDLE
 player_motion_state_v = IDLE
+OUCH = {'text':'OUCH', 'is visible':False}
+alien_lives = 3
+Hearts = [ ]
+Music_is_playing = False                
 
+for Heart_Counter in range(alien_lives):
+    heart = Actor("heart")
+    heart.pos = (100+50*Heart_Counter, 100)
+    heart.scale = 1/10
+    Hearts.append(heart)
 
 def shoot_bullet_player():
     bullet = bullet_create() 
@@ -34,14 +47,11 @@ def bullet_create ():
     new_bullet_x = (WIDTH/2) + distance_from_center * math.cos(angle_of_position)
     new_bullet_y = (HEIGHT/2) + distance_from_center * math.sin(angle_of_position)
 
-    
-
-
 
     # math.radians(), math.sin(), math.cos()
 
     # convert angle in degrees into angle in radians
-    player_x, player_y = alien.pos   #  if player.pos is (4, 6), then python will put 4 to playr_x and 6 to player_y
+    player_x, player_y = alien.pos   #  if player.pos is (4, 6), then python will put 4 to player_x and 6 to player_y
     
     new_bullet_speed_x = (player_x - new_bullet_x)
     new_bullet_speed_y = (player_y - new_bullet_y)
@@ -72,52 +82,108 @@ def bullet_create ():
 bullets = []
 clock.schedule(shoot_bullet_player,1)
 
+def draw_bullet(bullet):
+    for neighbor_offset_x in [-2, -1, 0, 1, 2]:
+        for neighbor_offset_y in [-2, -1, 0, 1, 2]:
+            # Drawing a bullet as line
+
+            # find a line start point
+            line_start_x = bullet['x'] + bullet['speed']['h'] + neighbor_offset_x
+            line_start_y = bullet['y'] + bullet['speed']['v'] + neighbor_offset_y
+
+            # find a line end point
+            line_end_x = bullet['x'] - bullet['speed']['h'] + neighbor_offset_x
+            line_end_y = bullet['y'] - bullet['speed']['v'] + neighbor_offset_y
+
+            # specify a color
+            bullet_color = (255, 10, 10)
+
+            # draw the line on screen finally
+            screen.draw.line((line_start_x, line_start_y),(line_end_x, line_end_y), bullet_color)
+
     
 def draw():
     screen.fill(blue)
+    if alien_lives > 0:
+        if OUCH['is visible']:
+            screen.draw.text(
+                OUCH['text'], # OUCH!!
+                pos=(alien.left, alien.bottom)
+            )   
+        for heart_counter in range(alien_lives):
+            Hearts[heart_counter].draw()
 
-
-    for bullet in bullets:
-        screen.draw.filled_rect(Rect((bullet['x'], bullet["y"]), (35, 10)), (255, 10, 10))
-
-    alien.draw()
-
-
+        for bullet in bullets:
+            # screen.draw.filled_rect(Rect((bullet['x'], bullet["y"]), (35, 10)), (255, 10, 10))
+            draw_bullet(bullet)
+        alien.draw()
+    else:
+        screen.draw.text('GAME OVER', pos=(WIDTH/2, HEIGHT/2))
+        
+    
+def OUCH_remove():
+    OUCH['is visible'] = False
+    alien.image = 'alien'
+    
 # Is called once per frame
 def update():
-    
-    global horizontal_direction
-    global vertical_direction
+    global alien_lives
+    global Music_is_playing
+    if alien_lives>0:   # Active game screen
 
-    if player_motion_state_v == MOVING:
-        if vertical_direction == "U":
-            alien.top -= Speed
-        else:
-            alien.top += Speed
-    if player_motion_state_h == MOVING:
-        if horizontal_direction == "L":
-            alien.left -= Speed
-        else:
-            alien.left += Speed
-
+        if not Music_is_playing:
+            music.set_volume(0.5)
+            music.play("engine_fire") 
+            Music_is_playing = True
             
-    for bullet in bullets:
-        bullet['y'] = bullet['y'] + bullet['speed']['v']
-        bullet['x'] = bullet['x'] + bullet['speed']['h']           # D = v * t
-                                                               # v = D / t
-                                                               # 
-                                                               # D_new = D_old + v * t     
-                                                               #    where
-                                                               #        t ---> frequency of the frame
-                                                               #        v ---> number of pixel per frame 
-    
-    # # If it goes beyond the right-hand side edge of the screen
-    # if alien.left > WIDTH:
-    #     direction = "L"
+        global horizontal_direction
+        global vertical_direction
 
-    # # If it goes beyond the left-hand side edge of the screen
-    # if alien.right < 0:
-    #     direction = "R"
+        if player_motion_state_v == MOVING:
+            if vertical_direction == "U":
+                alien.top -= Speed
+            else:
+                alien.top += Speed
+        if player_motion_state_h == MOVING:
+            if horizontal_direction == "L":
+                alien.left -= Speed
+            else:
+                alien.left += Speed
+    
+        
+                
+        for bullet in bullets:
+            bullet['y'] = bullet['y'] + bullet['speed']['v']
+            bullet['x'] = bullet['x'] + bullet['speed']['h']      
+        # D = v * t
+        # v = D / t
+        # D_new = D_old + v * t     
+        #    where
+        #        t ---> frequency of the frame
+        #        v ---> number of pixel per frame 
+        
+            if not OUCH['is visible'] and alien.collidepoint((bullet['x'], bullet['y'])):
+                print('OUCH')
+                OUCH['is visible'] = True 
+                clock.schedule(OUCH_remove,2)
+                alien.image = 'alien_hurt'
+                alien_lives = alien_lives-1
+                sounds.eep.play()
+                if alien_lives<=0:
+                    Music_is_playing = False
+
+        # # If it goes beyond the right-hand side edge of the screen
+        # if alien.left > WIDTH:
+        #     direction = "L"
+
+        # # If it goes beyond the left-hand side edge of the screen
+        # if alien.right < 0:
+        #     direction = "R"
+
+    else:   # Gamve over screen code
+        if not Music_is_playing:
+            music.play_once("mario_fail") 
+            Music_is_playing = True
         
 def on_key_down(key):
    
@@ -131,12 +197,14 @@ def on_key_down(key):
         # Change direction to Right
         horizontal_direction = "R"
         player_motion_state_h = MOVING
+        alien.flip_x = False
 
     # a.k. if the user presses left arrow key, then...
     if key == keys.LEFT:
           # ...change ot left
         horizontal_direction = "L"
         player_motion_state_h = MOVING
+        alien.flip_x = True
 
     if key == keys.UP:
         vertical_direction = "U"
@@ -155,4 +223,3 @@ def on_key_up(key):
     global player_motion_state_h
     if key == keys.LEFT or key == keys.RIGHT:
         player_motion_state_h = IDLE
-
