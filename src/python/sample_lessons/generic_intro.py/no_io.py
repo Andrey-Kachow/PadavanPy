8c1709f9fadf01659a2ca1e285c546de7fd756bc
Andrey-Kachow <andr10xp@gmail.com>
2024-08-20 19:11:51 +0100

    Added test no io approach

---
+++ src/python/sample_lessons/generic_intro.py/no_io.py
@@ -0,0 +1,16 @@
+'''
+    age = 18
+    country = "USA"
+    answer = "I don't know"
+
+    ########## Your Code ###########
+
+
+    if age < 18 or (country == "USA" and age < 21):
+        answer = "No"
+
+
+    ###### End of your code ########
+
+    print(answer)
+'''
