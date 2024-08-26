baf934067816e3f6217ffbb1bba8548eaf661b58
Andrey-Kachow <andr10xp@gmail.com>
2024-08-26 16:07:41 +0100

    First demo recorded

--- src/python/sample_lessons/generic_intro.py/refac_lec_for_n_def/Homework_4.py
+++ src/python/sample_lessons/generic_intro.py/refac_lec_for_n_def/Homework_4.py
@@ -14,41 +14,37 @@ answer = "I don't know"
 
 ################## Начало Вашего Кода #######################
 
-#---------------------------------############################### Проверка корректности date_1 переменных
-if date_1_month == 2:
-    if (date_1_year % 4 == 0 and date_1_year % 100 != 0) or (date_1_year % 400 == 0):
-        max_day = 29
+dates = [
+    (date_1_day, date_1_month, date_1_year),
+    (date_2_day, date_2_month, date_2_year)
+]
+
+date_validies = []
+
+for date in dates:
+    day, month, year = date
+    
+    if month == 2:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            max_day = 29
+        else:
+            max_day = 28
+    elif month in [4, 6, 9, 11]:
+        max_day = 30
     else:
-        max_day = 28
-elif date_1_month in [4, 6, 9, 11]:
-    max_day = 30
-else:
-    max_day = 31
+        max_day = 31
 
-if date_1_day >= 1 and date_1_day <= max_day and date_1_month >= 1 and date_1_month <= 12 and date_1_year >= 1:
-    date_1_is_valid = True
-else:
-    date_1_is_valid = False
-
-#--------------------########### Проверка корректности date_2 переменных
-
-if date_2_month == 2:
-    if (date_2_year % 4 == 0 and date_2_year % 100 != 0) or (date_2_year % 400 == 0):
-        max_day = 29
+    if day >= 1 and day <= max_day and month >= 1 and month <= 12 and year >= 1:
+        is_valid = True
     else:
-        max_day = 28
-elif date_2_month in [4, 6, 9, 11]:
-    max_day = 30
-else:
-    max_day = 31
+        is_valid = False
 
-if date_2_day >= 1 and date_2_day <= max_day and date_2_month >= 1 and date_2_month <= 12 and date_2_year >= 1:
-    date_2_is_valid = True
-else:
-    date_2_is_valid = False
+    date_validies = date_validies + [is_valid]
+
+    # date_validies.append(is_valid)
 
 #---------------------------------############################### Правильны ли даты?
-if date_1_is_valid and date_2_is_valid:
+if all(date_validies):
     date_1 = (date_1_year, date_1_month, date_1_day)
     date_2 = (date_2_year, date_2_month, date_2_day)
     if date_1 < date_2:
