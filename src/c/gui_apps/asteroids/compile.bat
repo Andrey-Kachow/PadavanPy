e481c3c00e1463ad8e38cbd0b0e97c5ed42acac5
Andrey-Kachow <andr10xp@gmail.com>
2024-10-31 13:55:02 +0000

    Little adjustment

--- src/c/gui_apps/asteroids/compile.bat
+++ src/c/gui_apps/asteroids/compile.bat
@@ -6,4 +6,5 @@ cl.exe /Zi ^
         "D:\____Developer_Libraries\SDL2-2.30.3\lib\x86\SDL2.lib" ^
         "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x86\shell32.lib" ^
     /SUBSYSTEM:CONSOLE
+    
     @REM /SUBSYSTEM:WINDOWS
