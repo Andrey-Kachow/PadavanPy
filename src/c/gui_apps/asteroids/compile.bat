679bfd969ca3e81df5ddbb4b0af1b7ea1822bbd3
Andrey-Kachow <andr10xp@gmail.com>
2024-11-06 11:50:37 +0000

    notes for compilation of multiple files

--- src/c/gui_apps/asteroids/compile.bat
+++ src/c/gui_apps/asteroids/compile.bat
@@ -8,3 +8,13 @@ cl.exe /Zi ^
     /SUBSYSTEM:CONSOLE
     
     @REM /SUBSYSTEM:WINDOWS
+
+    @REM cl.exe /Zi ^
+    @REM     /I "D:\____Developer_Libraries\SDL2-2.30.3\include" ^
+    @REM     main.c ^
+    @REM     example_extra_file.c ^
+    @REM     /link ^
+    @REM         "D:\____Developer_Libraries\SDL2-2.30.3\lib\x86\SDL2main.lib" ^
+    @REM         "D:\____Developer_Libraries\SDL2-2.30.3\lib\x86\SDL2.lib" ^
+    @REM         "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x86\shell32.lib" ^
+    @REM     /SUBSYSTEM:CONSOLE
