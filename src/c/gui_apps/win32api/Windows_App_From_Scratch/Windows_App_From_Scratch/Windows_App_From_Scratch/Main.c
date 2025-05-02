eb530963ad869be07f72469b568d714ee1dfa164
Andrey-Kachow <andr10xp@gmail.com>
2025-05-02 08:47:53 +0100

    Display the first window

--- src/c/gui_apps/win32api/Windows_App_From_Scratch/Windows_App_From_Scratch/Windows_App_From_Scratch/Main.c
+++ src/c/gui_apps/win32api/Windows_App_From_Scratch/Windows_App_From_Scratch/Windows_App_From_Scratch/Main.c
@@ -1,10 +1,18 @@
 ﻿#include <windows.h>
 
+const LPCWSTR MY_WINDOW_CLASS = L"myAmazingWindowClass";
+
 // 1. Basic signature
 // int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {}
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lparam) {
-
+    switch (message) {
+    case WM_DESTROY:
+        PostQuitMessage(0); // makes GetMessage return false;
+        break;
+    default:
+        return DefWindowProcW(hWnd, message, wParam, lparam);
+    }
 }
 
 int WINAPI WinMain(
@@ -14,18 +22,29 @@ int WINAPI WinMain(
     _In_ int nShowCmd
 ) {
     // 2. Dialog
-	MessageBox(NULL, L"Добро Пожаловать!", L"Привет", MB_OK);
+	//MessageBox(NULL, L"Добро Пожаловать!", L"Привет", MB_OK);
 
-    WNDCLASSW windowClass = {0};
+    // 3. Widnow Class
+    WNDCLASSW windowClass = { 0 };
     //
     windowClass.hbrBackground = COLOR_WINDOW;
     windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
     windowClass.hInstance = hInstance;
-    windowClass.lpszClassName = L"myAmazingWindowClass";
+    windowClass.lpszClassName = MY_WINDOW_CLASS;
     windowClass.lpfnWndProc = WindowProcedure;
-
+    //
     if (!RegisterClassW(&windowClass)) {
+        return -1;
+    }
+    
+    // 4. Creating a window
+    CreateWindow(MY_WINDOW_CLASS, L"Добро Пожаловать в мое Окно", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
 
+    // 5. Message Loop (otherwise exits immediately)
+    MSG message = { 0 };
+    while (GetMessage(&message, NULL, NULL, NULL)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
     }
 
 	return 0;
